7eb43f33a070d730bd3412601f4711f329eb3da1
Willian Zanuto Oliveira <willian.oliveira@unicampo.coop.br>
2023-06-19 17:33:37 -0300

    Validações de teste

--- TestApiWebFilmeAplication/UnitTest1.cs
+++ TestApiWebFilmeAplication/UnitTest1.cs
@@ -42,6 +42,26 @@ namespace TestApiWebFilmeApplication
             Assert.AreEqual(2, premiosResponse.Min.Count);
             Assert.IsNotNull(premiosResponse.Max);
             Assert.AreEqual(2, premiosResponse.Max.Count);
+
+            Assert.AreEqual("Gloria Katz", premiosResponse.Min[0].Producer);
+            Assert.AreEqual(0, premiosResponse.Min[0].Interval);
+            Assert.AreEqual(1986, premiosResponse.Min[0].PreviousWin);
+            Assert.AreEqual(1986, premiosResponse.Min[0].FollowingWin);
+
+            Assert.AreEqual("Steven Perry and Joel Silver", premiosResponse.Min[1].Producer);
+            Assert.AreEqual(0, premiosResponse.Min[1].Interval);
+            Assert.AreEqual(1990, premiosResponse.Min[1].PreviousWin);
+            Assert.AreEqual(1990, premiosResponse.Min[1].FollowingWin);
+
+            Assert.AreEqual("Mario Kassar, Joel B. Michaels and Andrew G. Vajna", premiosResponse.Max[0].Producer);
+            Assert.AreEqual(2, premiosResponse.Max[0].Interval);
+            Assert.AreEqual(2006, premiosResponse.Max[0].PreviousWin);
+            Assert.AreEqual(2008, premiosResponse.Max[0].FollowingWin);
+
+            Assert.AreEqual("Allan Carr", premiosResponse.Max[1].Producer);
+            Assert.AreEqual(1, premiosResponse.Max[1].Interval);
+            Assert.AreEqual(1980, premiosResponse.Max[1].PreviousWin);
+            Assert.AreEqual(1981, premiosResponse.Max[1].FollowingWin);
         }
 
     }
